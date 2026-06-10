700531725a6c2eb910a489fedbd5d41bbe7169bf
tamilselvan182425 <tamilselvanppy18@gmail.com>
2026-06-10 13:13:59 +0530

    i add .env file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,18 @@ const api = axios.create({
     baseURL: 'http://localhost:5000/api',
 });
 
+const API_URL = import.meta.env.API_URL;
+
+export const getData = async () => {
+    const response = await fetch(`${API_URL}/api/test`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return response.json();
+};
+
 // Add a request interceptor to include the JWT token
 api.interceptors.request.use(
     (config) => {
